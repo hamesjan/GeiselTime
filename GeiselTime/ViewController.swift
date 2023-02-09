829423ecd5f362a1b119552d1aaac1fc4803277e
hamesjan <jhan31903@gmail.com>
2023-02-08 23:27:30 -0800

    Initial Commit

---
+++ GeiselTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeiselTime
+//
+//  Created by James Jongyoon Han on 2/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
